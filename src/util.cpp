41d5767fdd81ab5e84690cbcb77562ebded99e97
himanshu4865 <himanshu@prologictechnologies.in>
2018-05-30 18:07:09 +0530

    revert some changes

--- src/util.cpp
+++ src/util.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2017 The Zuzcoin Core developers
+// Copyright (c) 2009-2017 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -949,7 +949,7 @@ std::string CopyrightHolders(const std::string& strPrefix)
 
     // Check for untranslated substitution to make sure Zuzcoin Core copyright is not removed by accident
     if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION).find("Zuzcoin Core") == std::string::npos) {
-        strCopyrightHolders += "\n" + strPrefix + "The Zuzcoin Core developers";
+        strCopyrightHolders += "\n" + strPrefix + "The Bitcoin Core developers";
     }
     return strCopyrightHolders;
 }
