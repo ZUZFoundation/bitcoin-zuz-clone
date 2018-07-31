c591066086b1ae9a2d1dd216504c8df7efc9aa4a
himanshu4865 <himanshu@prologictechnologies.in>
2018-07-31 16:02:37 +0530

    maturity set to 200 blocks and commenting premining address of client server

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -161,7 +161,7 @@ public:
         consensus.zuzPowDGWHeight = 400;
         consensus.zuzPremineEnforcePubKeys = true;
         zuzPreminePubkeys =
-        {"MJ2UNWH1jHwabJPSnq9gdZ9RHDNCgJTDLn"};
+        {"MJ2UNWH1jHwabJPSnq9gdZ9RHDNCgJTDLn"}; //nico server : MWCJp4Pg4UhCEbn5PKKK9eJyFsWCmDe8V6;
 
         consensus.BIP16Height = 173805; // 00000000000000ce80a7e057163a4db1d5ad7b20fb6f598c9597b9665c8fb0d4 - April 1, 2012
         consensus.BIP34Height = 227931;
